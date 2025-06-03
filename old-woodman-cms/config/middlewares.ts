662f0f0292cd7c327c8f22696e13bf2403ddd25a
web-developer108 <o.belaya108@gmale.com>
2025-06-03 12:46:19 +0300

    settings | instaled cms again

---
+++ old-woodman-cms/config/middlewares.ts
@@ -0,0 +1,18 @@
+module.exports = [
+  'strapi::errors',
+  'strapi::security',
+  {
+    name: 'strapi::cors',
+    config: {
+      origin: ['http://localhost:5173', 'https://old-woodman.vercel.app'],
+      credentials: true,
+    },
+  },
+  'strapi::poweredBy',
+  'strapi::logger',
+  'strapi::query',
+  'strapi::body',
+  'strapi::session',
+  'strapi::favicon',
+  'strapi::public',
+];
