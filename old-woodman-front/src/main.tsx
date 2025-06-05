6a8a37b5bbded5b62afd5e82eee6415fb079510d
web-developer108 <o.belaya108@gmale.com>
2025-06-05 11:28:24 +0300

    preparation | installed sass and added styles.scss

--- old-woodman-front/src/main.tsx
+++ old-woodman-front/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import App from './App';
+import './styles.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- old-woodman-front/src/pages/temporary/temporary.tsx
+++ old-woodman-front/src/pages/temporary/temporary.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import '../../App.css'
 
 type Product = {
   id: number
