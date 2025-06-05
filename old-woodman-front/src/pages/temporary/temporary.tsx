56e586231343207cc2f834b97ad3dee7d8727c87
web-developer108 <o.belaya108@gmale.com>
2025-06-05 07:01:34 +0300

    preparation | corrected to show homepage

--- old-woodman-front/src/pages/temporary/temporary.tsx
+++ old-woodman-front/src/pages/temporary/temporary.tsx
@@ -1,7 +1,5 @@
 import { useEffect, useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import '../../App.css'
 
 type Product = {
   id: number
@@ -36,10 +34,8 @@ const Temporary=()=> {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo"/>
         </a>
         <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo"/>
         </a>
       </div>
       <h1>Vite + React</h1>
--- old-woodman-front/src/routes/routes.tsx
+++ old-woodman-front/src/routes/routes.tsx
@@ -1,33 +1,34 @@
 import { lazy } from 'react';
 import type { RouteObject } from 'react-router-dom';
+import Temporary from '../pages/temporary/temporary';
 
-const Home = lazy(() => import('../pages/temporary/temporary.tsx'));
-
+//const Home = lazy(() => import('../pages/temporary/temporary'));
+const Home = Temporary;
 // Doors
-const DoorsOverview = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Classica = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Loft = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Balcony = lazy(() => import('../pages/temporary/temporary.tsx'));
-const DoorCollectionItem = lazy(() => import('../pages/temporary/temporary.tsx'));
+const DoorsOverview = lazy(() => import('../pages/temporary/temporary'));
+const Classica = lazy(() => import('../pages/temporary/temporary'));
+const Loft = lazy(() => import('../pages/temporary/temporary'));
+const Balcony = lazy(() => import('../pages/temporary/temporary'));
+const DoorCollectionItem = lazy(() => import('../pages/temporary/temporary'));
 
 // Furniture
-const FurnitureOverview = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Consoles = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Wardrobes = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Beds = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Chairs = lazy(() => import('../pages/temporary/temporary.tsx'));
-const FurnitureCollectionItem = lazy(() => import('../pages/temporary/temporary.tsx'));
+const FurnitureOverview = lazy(() => import('../pages/temporary/temporary'));
+const Consoles = lazy(() => import('../pages/temporary/temporary'));
+const Wardrobes = lazy(() => import('../pages/temporary/temporary'));
+const Beds = lazy(() => import('../pages/temporary/temporary'));
+const Chairs = lazy(() => import('../pages/temporary/temporary'));
+const FurnitureCollectionItem = lazy(() => import('../pages/temporary/temporary'));
 
 // Other pages
-const Facades = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Gifts = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Promotions = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Contacts = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Cart = lazy(() => import('../pages/temporary/temporary.tsx'));
-const FAQ = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Info = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Terms = lazy(() => import('../pages/temporary/temporary.tsx'));
-const Favorites = lazy(() => import('../pages/temporary/temporary.tsx'));
+const Facades = lazy(() => import('../pages/temporary/temporary'));
+const Gifts = lazy(() => import('../pages/temporary/temporary'));
+const Promotions = lazy(() => import('../pages/temporary/temporary'));
+const Contacts = lazy(() => import('../pages/temporary/temporary'));
+const Cart = lazy(() => import('../pages/temporary/temporary'));
+const FAQ = lazy(() => import('../pages/temporary/temporary'));
+const Info = lazy(() => import('../pages/temporary/temporary'));
+const Terms = lazy(() => import('../pages/temporary/temporary'));
+const Favorites = lazy(() => import('../pages/temporary/temporary'));
 
 const routes: RouteObject[] = [
   { path: '/', element: <Home /> },
