3a8740fb0cfb9db67f960bdd351a1223db585777
web-developer108 <o.belaya108@gmale.com>
2025-06-12 15:37:20 +0300

    home-page | added scroll-to-top component

--- old-woodman-front/src/main.tsx
+++ old-woodman-front/src/main.tsx
@@ -7,17 +7,19 @@ import { CartProvider } from './hooks/cart/cart.tsx';
 import { FavoritesProvider } from './hooks/favorites/favorites.tsx';
 import './i18n/index.ts';
 import './styles.scss';
+import { ScrollToTop } from './components/scroll-to-top/scroll-to-top.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-      <BrowserRouter>
-        <DeviceProvider>
-          <CartProvider>
-            <FavoritesProvider>
-              <App/>
-            </FavoritesProvider>
-          </CartProvider>
-        </DeviceProvider>
-      </BrowserRouter>
+    <BrowserRouter>
+      <ScrollToTop/>
+      <DeviceProvider>
+        <CartProvider>
+          <FavoritesProvider>
+            <App/>
+          </FavoritesProvider>
+        </CartProvider>
+      </DeviceProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
