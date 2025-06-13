2df89744657c020be9bd6036239fe899ddd89363
web-developer108 <o.belaya108@gmale.com>
2025-06-13 12:12:18 +0300

    home-page | added background colors

--- old-woodman-front/src/styles.ts
+++ old-woodman-front/src/styles.ts
@@ -1,6 +1,8 @@
 export const AppColors = {
   text: {
     main: '#222222',
+    main25: 'rgba(34, 34, 34, 0.25)',
+    main50: 'rgba(34, 34, 34, 0.50)',
     light: '#ffffff',
     grey: '#666666',
   },
@@ -11,6 +13,10 @@ export const AppColors = {
   },
   background: {
     grey: '#f4f4f4',
+    brown: '#69423a',
+    blue: '#4384c0',
+    yellow: '#ffe0a8',
+    orange: '#e38535',
     circleButton : '#e3e3e3',
     circleButton40 : ' rgba(0, 0, 0, 0.50)',
 
