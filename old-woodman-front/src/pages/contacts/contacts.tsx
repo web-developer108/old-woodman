795003f473d18906f64231ec242c64c5c0d70d5f
web-developer108 <o.belaya108@gmale.com>
2025-06-21 13:43:00 +0300

    contacts-page | added layout

--- old-woodman-front/src/pages/contacts/contacts.tsx
+++ old-woodman-front/src/pages/contacts/contacts.tsx
@@ -3,6 +3,8 @@ import { ToolPageLayout } from '../../components/tool-page-layout/tool-page-layo
 import { Breadcrumbs } from '../../components/breadcrumbs/breadcrumbs.tsx';
 import styles from './contacts.module.scss'
 import useDevice from '../../hooks/device/use-device.ts';
+import { ColorButton } from '../../components/buttons/color-button/color-button.tsx';
+import { ArrowTopRightIcon } from '../../components/icons/arrow-top-right-icon/arrow-top-right-icon.tsx';
 
 const Contacts = () => {
   const { t } = usePageTranslate();
@@ -26,11 +28,22 @@ const Contacts = () => {
           </h1>
           <div className={styles.infoContainer}>
             <div className={styles.infoWrap}>
-
+              <h2>{t('section-title-1').toUpperCase()}</h2>
+              <span>{t('section-description-1')}</span>
             </div>
-
+            <div className={styles.infoWrap}>
+              <h2>{t('section-title-2').toUpperCase()}</h2>
+              <span>{t('section-description-2')}</span>
+            </div>
+            <div className={styles.infoWrap}>
+              <h2>{t('section-title-3').toUpperCase()}</h2>
+              <span>{t('section-description-3')}</span>
+            </div>
           </div>
-        </div>
+          <div className={styles.buttonWrapper}>
+          <ColorButton icon = {<ArrowTopRightIcon/>} label = {t('button-label')}/>
+          </div>
+          </div>
       </div>
     </ToolPageLayout>
   )
