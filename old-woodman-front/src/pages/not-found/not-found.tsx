daedbae13f72b8aa92ab525ac75b43d4ca485cf5
web-developer108 <o.belaya108@gmale.com>
2025-06-25 14:39:00 +0300

    page404 | added social buttons

--- old-woodman-front/src/pages/not-found/not-found.tsx
+++ old-woodman-front/src/pages/not-found/not-found.tsx
@@ -5,22 +5,48 @@ import { ArrowRightIcon } from '../../components/icons/arrow-right-icon/arrow-ri
 import {AppColors} from '../../styles.ts';
 import styles from './not-found.module.scss';
 import { NotFoundIcon } from '../../components/icons/not-found-icon/not-found-icon.tsx';
+import { CircleButton } from '../../components/buttons/circle-button/circle-button.tsx';
+import { WhatsappIcon } from '../../components/icons/whatsapp-icon/whatsapp-icon.tsx';
+import { TelegramIcon } from '../../components/icons/telegram-icon/telegram-icon.tsx';
 
 const NotFound = () => {
   const {t} = useTranslation('common');
  return (
    <ToolPageLayout>
-  <div className={styles.notFoundContainer}>
-    <h1>{t('not-found.title').toUpperCase()}</h1>
-    <div>{t('not-found.description')}</div>
-    <div className={styles.buttonWrap}>
-      <ColorButton label ={t('not-found.button')} icon = {<ArrowRightIcon color={AppColors.text.main} size = 'small'/>}/>
-    </div>
-    <div className={styles.iconWrap}>
-      <NotFoundIcon/>
-    </div>
-  </div>
+     <div className={styles.notFoundContainer}>
+       <h1>{t('not-found.title').toUpperCase()}</h1>
+       <div>{t('not-found.description')}</div>
+       <div className={styles.buttonWrap}>
+         <ColorButton label={t('not-found.button')} icon={<ArrowRightIcon color={AppColors.text.main} size='small'/>}/>
+       </div>
+       <div className={styles.iconWrap}>
+         <NotFoundIcon/>
+       </div>
+     </div>
+     <div className={styles.circleButtons}>
+       <a
+         href="https://wa.me/77081826004"
+         target="_blank"
+         rel="noopener noreferrer"
+         aria-label="WhatsApp"
+       >
+         <CircleButton bgColor={AppColors.button.green} icon={<WhatsappIcon color={AppColors.text.light}/>}/>
+       </a>
+       <a
+         href="https://wa.me/77081826004"
+         target="_blank"
+         rel="noopener noreferrer"
+         aria-label="WhatsApp"
+       >
+         <CircleButton bgColor={AppColors.button.blue} icon={
+
+           <TelegramIcon
+             arrowColor={AppColors.button.blue}
+             backgroundColor={AppColors.text.light}/>}/>
+       </a>
+     </div>
+
    </ToolPageLayout>
-)
+ )
 };
 export default NotFound;
