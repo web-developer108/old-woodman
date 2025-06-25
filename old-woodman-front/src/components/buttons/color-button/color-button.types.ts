ba28791663d3eec37db1973f563ab42a36b31091
web-developer108 <o.belaya108@gmale.com>
2025-06-25 17:22:42 +0300

    gifts-page | added black color to color-button

--- old-woodman-front/src/components/buttons/color-button/color-button.types.ts
+++ old-woodman-front/src/components/buttons/color-button/color-button.types.ts
@@ -1,6 +1,6 @@
 import React from 'react';
 
-type ColorButtonVariant = 'yellow' | 'green' | 'blue';
+type ColorButtonVariant = 'yellow' | 'green' | 'blue' | 'black';
 
 export type ColorButtonProps = {
   label: string;
