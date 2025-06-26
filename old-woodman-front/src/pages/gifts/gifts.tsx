caeb0a0f6799bfb061953a710490511999edfded
web-developer108 <o.belaya108@gmale.com>
2025-06-26 10:28:34 +0300

    gifts-page | added article

--- old-woodman-front/src/pages/gifts/gifts.tsx
+++ old-woodman-front/src/pages/gifts/gifts.tsx
@@ -3,6 +3,7 @@ import styles from './gifts.module.scss'
 import { usePageTranslate } from '../../hooks/page-translate/page-translate.ts';
 import { RunningText } from '../../components/running-text/running-text.tsx';
 import { SectionTabs } from '../../components/section-tabs/section-tabs.tsx';
+import { Breadcrumbs } from '../../components/breadcrumbs/breadcrumbs.tsx';
 
 const Gifts = () => {
   const { t } = usePageTranslate();
@@ -16,7 +17,28 @@ const Gifts = () => {
           </div>
         </div>
         <RunningText/>
+        <div className={styles.giftsContent}>
+
+        <Breadcrumbs current = {t('main-header')}/>
         <SectionTabs/>
+          <section className={styles.article}>
+            <h2 className={styles.infoTitle}>{t('info-title').toUpperCase()}</h2>
+            <div className={styles.columns}>
+              <div className={styles.column}>
+                <span>{t('info-text-1')}</span>
+                <span>{t('info-text-2')}</span>
+                <span>{t('info-text-3')}</span>
+              </div>
+              <div className={styles.column}>
+                <span>{t('info-text-4')}</span>
+                <span>{t('info-text-5')}</span>
+              </div>
+            </div>
+          </section>
+          <div>
+
+          </div>
+        </div>
       </div>
     </ToolPageLayout>
   )
