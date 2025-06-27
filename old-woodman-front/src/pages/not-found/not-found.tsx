456a69c290e595e790d6b2600b6198f4f7cc9dbb
web-developer108 <o.belaya108@gmale.com>
2025-06-27 17:05:42 +0300

    corrections | added social buttons to not-found component

--- old-woodman-front/src/pages/not-found/not-found.tsx
+++ old-woodman-front/src/pages/not-found/not-found.tsx
@@ -4,9 +4,7 @@ import { ToolPageLayout } from '../../components/tool-page-layout/tool-page-layo
 import { ColorButton } from '../../components/buttons/color-button/color-button.tsx';
 import { ArrowRightIcon } from '../../components/icons/arrow-right-icon/arrow-right-icon.tsx';
 import { NotFoundIcon } from '../../components/icons/not-found-icon/not-found-icon.tsx';
-import { CircleButton } from '../../components/buttons/circle-button/circle-button.tsx';
-import { WhatsappIcon } from '../../components/icons/whatsapp-icon/whatsapp-icon.tsx';
-import { TelegramIcon } from '../../components/icons/telegram-icon/telegram-icon.tsx';
+import { SocialButtons } from '../../components/buttons/social-buttons/social-buttons.tsx';
 import { AppColors } from '../../styles.ts';
 import styles from './not-found.module.scss';
 
@@ -28,26 +26,7 @@ const NotFound = () => {
         </div>
       </div>
       <div className={styles.circleButtons}>
-        <a
-          href="https://wa.me/77081826004"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="WhatsApp"
-        >
-          <CircleButton ariaLabel={t('common:social.aria-label.wa')} bgColor={AppColors.button.green} icon={<WhatsappIcon color={AppColors.text.light}/>}/>
-        </a>
-        <a
-          href="https://wa.me/77081826004"
-          target="_blank"
-          rel="noopener noreferrer"
-          aria-label="WhatsApp"
-        >
-          <CircleButton ariaLabel={t('common:social.aria-label.t')} bgColor={AppColors.button.blue} icon={
-
-            <TelegramIcon
-              arrowColor={AppColors.button.blue}
-              backgroundColor={AppColors.text.light}/>}/>
-        </a>
+        <SocialButtons/>
       </div>
 
     </ToolPageLayout>
