c10c0e3872ab3beb809af04df91a84535e89ab37
web-developer108 <o.belaya108@gmale.com>
2025-06-30 15:36:31 +0300

    facades-page | added article

--- old-woodman-front/src/pages/facades/facades.tsx
+++ old-woodman-front/src/pages/facades/facades.tsx
@@ -11,41 +11,87 @@ import useDevice from '../../hooks/device/use-device.ts';
 
 const Facades = () => {
   const { t } = usePageTranslate();
-  const { isMobile} = useDevice();
-/*  const galleryImages = [
-    { src: clock, alt: 'Часы' },
-    { src:mirror, alt: 'Зеркало' },
+  const { isMobile } = useDevice();
+  /*  const galleryImages = [
+      { src: clock, alt: 'Часы' },
+      { src:mirror, alt: 'Зеркало' },
 
-  ];*/
+    ];*/
   return (
     <ToolPageLayout>
       <div className={styles.facadesContainer}>
         <div className={styles.imageContainer}>
           <div className={styles.imageTitleContainer}>
-            <h1 className={styles.imageTitle} dangerouslySetInnerHTML={{__html: isMobile ?t('main-header-mobile').toUpperCase() : t('main-header').toUpperCase()}}/>
+            <h1 className={styles.imageTitle}
+                dangerouslySetInnerHTML={{ __html: isMobile ? t('main-header-mobile').toUpperCase() : t('main-header').toUpperCase() }}/>
             <div className={styles.imageLabel}>{t('main-header.label')}</div>
           </div>
         </div>
         <RunningText/>
-        <div className={styles.giftsContent}>
+        <div className={styles.facadesContent}>
 
-          <Breadcrumbs current={t('main-header')}/>
+          <Breadcrumbs current={t('breadcrumbs.label')}/>
           <SectionTabs/>
           <section className={styles.article}>
-            <h2 className={styles.infoTitle}>{t('info-title').toUpperCase()}</h2>
             <div className={styles.columns}>
               <div className={styles.column}>
-                <span>{t('info-text-1')}</span>
-                <span>{t('info-text-2')}</span>
-                <span>{t('info-text-3')}</span>
+                <h2
+                  className={styles.infoTitle}>{isMobile ? t('info-title-1-mobile').toUpperCase() : t('info-title-1-desktop').toUpperCase()}</h2>
+                {isMobile ? (
+                  <span>{t('info-text-1-mobile')}</span>
+                ) : (
+                  <span>{t('info-text-1-desktop')}</span>
+                )}
+                <h2
+                  className={styles.infoTitle}>{t('info-title-2').toUpperCase() }</h2>
+                {isMobile ? (<>
+                  <span>{t('info-text-2.1-mobile')}</span>
+                  <span>{t('info-text-2.2-mobile')}</span>
+                  </>
+                ) : (
+                  <>
+                    <span>{t('info-text-2.1-desktop')}</span>
+                    <span>{t('info-text-2.2-desktop')}</span>
+                    <span>{t('info-text-2.3-desktop')}</span>
+                    <span>{t('info-text-2.4-desktop')}</span>
+                  </>
+                )}
               </div>
               <div className={styles.column}>
-                <span>{t('info-text-4')}</span>
-                <span>{t('info-text-5')}</span>
+                <h2
+                  className={styles.infoTitle}>{t('info-title-3').toUpperCase() }</h2>
+                {isMobile ? (<>
+                    <span>{t('info-text-3.1-mobile')}</span>
+                    <span>{t('info-text-3.2-mobile')}</span>
+                    <span>{t('info-text-3.3-mobile')}</span>
+                  </>
+                ) : (
+                  <>
+                    <span>{t('info-text-3.1-desktop')}</span>
+                    <span>{t('info-text-3.2-desktop')}</span>
+                    <span>{t('info-text-3.3-desktop')}</span>
+                    <span>{t('info-text-3.4-desktop')}</span>
+                  </>
+                )}
+                <h2
+                  className={styles.infoTitle}>{t('info-title-4').toUpperCase() }</h2>
+                {isMobile ? (<>
+                    <span>{t('info-text-4.1-mobile')}</span>
+                    <span>{t('info-text-4.2-mobile')}</span>
+                    <span>{t('info-text-4.3-mobile')}</span>
+                  </>
+                ) : (
+                  <>
+                    <span>{t('info-text-4.1-desktop')}</span>
+                    <span>{t('info-text-4.2-desktop')}</span>
+                    <span>{t('info-text-4.3-desktop')}</span>
+                    <span>{t('info-text-4.4-desktop')}</span>
+                  </>
+                )}
               </div>
             </div>
           </section>
-         {/* <Gallery images={galleryImages} layout={'two-large'}/>*/}
+          {/* <Gallery images={galleryImages} layout={'two-large'}/>*/}
           <section className={styles.textInfo}>
             <TextInfo/>
           </section>
