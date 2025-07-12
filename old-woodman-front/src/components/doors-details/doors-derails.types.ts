e070acb22809c3a9d6381d065734e5a35830a8ec
web-developer108 <o.belaya108@gmale.com>
2025-07-12 04:25:45 +0300

    classica-page | removed props from doors-detailes

--- old-woodman-front/src/components/doors-details/doors-derails.types.ts
+++
@@ -1,7 +0,0 @@
-import type { ProductCollection, ProductItem } from '../../config/config.types.ts';
-
-export interface DoorsDetailsProps {
-  collection: ProductCollection;
-  product: ProductItem;
-  lang: 'ru' | 'kk';
-}
--- old-woodman-front/src/components/doors-details/doors-details.tsx
+++ old-woodman-front/src/components/doors-details/doors-details.tsx
@@ -1,32 +1,41 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import type { DoorsDetailsProps } from './doors-derails.types.ts';
 import { useCart } from '../../hooks/cart/cart.tsx';
 import { LikeButton } from '../buttons/like-button/like-button.tsx';
 import { ImageSlider } from '../image-slider/image-slider.tsx';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams, useSearchParams } from 'react-router-dom';
 import useDevice from '../../hooks/device/use-device.ts';
+import { productCatalog } from '../../config/products.config.ts';
 import styles from './doors-details.module.scss';
 
-export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product, lang }) => {
-  const { t } = useTranslation('doors');
+export const DoorsDetails :React.FC = () => {
+  const { t, i18n } = useTranslation('doors');
   const { isMobile } = useDevice();
   const { isInCart, addToCart, removeFromCart } = useCart();
   const navigate = useNavigate();
   const [isExpanded, setIsExpanded] = useState(false);
   const [isOverflowing, setIsOverflowing] = useState(false);
   const textRef = useRef<HTMLDivElement>(null);
+  const { collectionId } = useParams();
+  const [searchParams] = useSearchParams();
+  const productId = searchParams.get('productId');
+  const lang = i18n.language as 'ru' | 'kk';
+  const collection = productCatalog
+    .find((cat) => cat.id === 'doors')
+    ?.collections?.find((col) => col.id === collectionId);
+
   const items = collection?.items || [];
-  const selectedProduct = product.id
-    ? items.find((item) => item.id === product.id) || items[0]
+
+  const selectedProduct = productId
+    ? items.find((item) => item.id === productId) || items[0]
     : items[0];
   const text = 'Три сценария работы:\n' +
     '            1.\tВы можете выбрать дверь или мебель из нашей галереи и посчитать у нас стоимость изготовления по вашим параметрам: размер, цвет, фурнитура.\n' +
     '            2.\tВы можете отправить нам эскиз двери или мебели, указать размеры и желаемый цвет, а мы посчитаем стоимость производства.\n' +
-    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
-    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
-    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
-    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n' +
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n' +
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n' +
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n' +
     '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'
 
   useEffect(() => {
@@ -46,36 +55,36 @@ export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product,
     window.addEventListener('resize', checkOverflow);
     return () => window.removeEventListener('resize', checkOverflow);
   }, [isMobile]);
-
+  if (!collection || !selectedProduct) return null;
   return (
     <div className={styles.page}>
       <div className={styles.imagesWrap}>
         <div className={styles.imageBlock}>
           <img
-            src={product.images[0]}
-            alt={product.title[lang]}
+            src={selectedProduct.images[0]}
+            alt={selectedProduct.title[lang]}
             className={styles.mainImage}
           />
           <div className={styles.likeWrap}>
-            <LikeButton productId={product.id}/>
+            <LikeButton productId={selectedProduct.id}/>
           </div>
 
         </div>
         <div className={styles.sliderContainer}>
-        <ImageSlider
-          selectedIndex={items.findIndex((i) => i.id === selectedProduct.id)}
-          onSelect={(index) => {
-            const selected = collection.items[index];
-            navigate(`/doors/${collection.id}?productId=${selected.id}`);
-          }}
-        />
+          <ImageSlider
+            selectedIndex={items.findIndex((i) => i.id === selectedProduct.id)}
+            onSelect={(index) => {
+              const selected = collection.items[index];
+              navigate(`/doors/${collection.id}?productId=${selected.id}`);
+            }}
+          />
         </div>
       </div>
       <div className={styles.infoBlock}>
         <div className={styles.titleBlock}>
           <h1 className={styles.title}>{collection.pageTitle?.[lang]}</h1>
           <div className={styles.price}>
-            {t('price-label')} {product.price.toLocaleString()} ₸*
+            {t('price-label')} {selectedProduct.price.toLocaleString()} ₸*
             <div className={styles.tooltipWrapper}>
               <div className={styles.icon}>i</div>
               <div className={styles.tooltip}>{t('info-text')}</div>
@@ -98,21 +107,21 @@ export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product,
           {isOverflowing &&
             !isExpanded &&
             (
-            <button className={styles.readMoreButton} onClick={() => setIsExpanded(true)}>
-              Читать дальше
-            </button>
-          )}
+              <button className={styles.readMoreButton} onClick={() => setIsExpanded(true)}>
+                Читать дальше
+              </button>
+            )}
         </div>
 
         <div className={styles.actions}>
 
           <button
             onClick={() =>
-              isInCart(product.id) ? removeFromCart(product.id) : addToCart(product.id)
+              isInCart(selectedProduct.id) ? removeFromCart(selectedProduct.id) : addToCart(productId as string)
             }
             className={styles.cartButton}
           >
-            {isInCart(product.id) ? t('in-cart') : t('add-to-cart')}
+            {isInCart(selectedProduct.id) ? t('in-cart') : t('add-to-cart')}
           </button>
         </div>
       </div>
