57f7c0a9d4a7c2668c12751ecf2188523deb54e4
web-developer108 <o.belaya108@gmale.com>
2025-07-12 04:08:16 +0300

    classica-page | added text block

--- old-woodman-front/src/components/doors-details/doors-details.tsx
+++ old-woodman-front/src/components/doors-details/doors-details.tsx
@@ -1,21 +1,52 @@
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import type { DoorsDetailsProps } from './doors-derails.types.ts';
 import { useCart } from '../../hooks/cart/cart.tsx';
 import { LikeButton } from '../buttons/like-button/like-button.tsx';
 import { ImageSlider } from '../image-slider/image-slider.tsx';
 import { useNavigate } from 'react-router-dom';
+import useDevice from '../../hooks/device/use-device.ts';
 import styles from './doors-details.module.scss';
 
 export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product, lang }) => {
   const { t } = useTranslation('doors');
+  const { isMobile } = useDevice();
   const { isInCart, addToCart, removeFromCart } = useCart();
   const navigate = useNavigate();
-
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isOverflowing, setIsOverflowing] = useState(false);
+  const textRef = useRef<HTMLDivElement>(null);
   const items = collection?.items || [];
   const selectedProduct = product.id
     ? items.find((item) => item.id === product.id) || items[0]
     : items[0];
+  const text = 'Три сценария работы:\n' +
+    '            1.\tВы можете выбрать дверь или мебель из нашей галереи и посчитать у нас стоимость изготовления по вашим параметрам: размер, цвет, фурнитура.\n' +
+    '            2.\tВы можете отправить нам эскиз двери или мебели, указать размеры и желаемый цвет, а мы посчитаем стоимость производства.\n' +
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'+
+    '            3.\tВы можете описать стиль вашего интерьера, а мы предложим подходящие варианты дверей и предметов мебели и посчитаем стоимость.\n'
+
+  useEffect(() => {
+    const checkOverflow = () => {
+      const el = textRef.current;
+      if (!el) return;
+      const maxHeight = isMobile ? 202 : 405;
+
+      const overflowing = el.scrollHeight > maxHeight;
+      console.log('scrollHeight:', el.scrollHeight);
+      console.log('maxHeight:', maxHeight);
+      console.log('isMobile:', isMobile);
+      setIsOverflowing(overflowing);
+    };
+
+    checkOverflow();
+    window.addEventListener('resize', checkOverflow);
+    return () => window.removeEventListener('resize', checkOverflow);
+  }, [isMobile]);
+
   return (
     <div className={styles.page}>
       <div className={styles.imagesWrap}>
@@ -42,7 +73,7 @@ export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product,
       </div>
       <div className={styles.infoBlock}>
         <div className={styles.titleBlock}>
-        <h1 className={styles.title}>{collection.pageTitle?.[lang]}</h1>
+          <h1 className={styles.title}>{collection.pageTitle?.[lang]}</h1>
           <div className={styles.price}>
             {t('price-label')} {product.price.toLocaleString()} ₸*
             <div className={styles.tooltipWrapper}>
@@ -52,10 +83,28 @@ export const DoorsDetails: React.FC<DoorsDetailsProps> = ({ collection, product,
           </div>
 
         </div>
-        <p className={styles.description}>{product.description[lang]}</p>
+        <div className={styles.descriptionContainer}>
+          <div
+            ref={textRef}
+            className={[
+              styles.textBlock,
+              isExpanded ? styles.expanded : '',
+              isOverflowing && !isExpanded ? styles.showFade : ''
+            ].join(' ')}
+          >
+            {text}
+          </div>
+
+          {isOverflowing &&
+            !isExpanded &&
+            (
+            <button className={styles.readMoreButton} onClick={() => setIsExpanded(true)}>
+              Читать дальше
+            </button>
+          )}
+        </div>
 
         <div className={styles.actions}>
-          <LikeButton productId={product.id}/>
 
           <button
             onClick={() =>
--- old-woodman-front/src/hooks/device/device.provider.tsx
+++ old-woodman-front/src/hooks/device/device.provider.tsx
@@ -1,23 +1,25 @@
-import React, { useEffect, useLayoutEffect, useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { DeviceContext, type Device, type IDeviceContext } from './device-context';
+
 
 const DeviceProvider = (props: React.PropsWithChildren) => {
   const [screenWidth, setScreenWidth] = useState<number>(0);
   const [device, setDevice] = useState<Device>(null);
 
   useLayoutEffect(() => {
-    const updateSize = () => setScreenWidth(window.innerWidth);
+    const updateSize = () => {
+      const width = window.innerWidth;
+      setScreenWidth(width);
+      setDevice(
+        width <= 810 ? 'mobile' :
+          width <= 1200 ? 'tablet' : 'desktop'
+      );
+    };
+
+    updateSize(); // инициализируем сразу
     window.addEventListener('resize', updateSize);
-    updateSize();
     return () => window.removeEventListener('resize', updateSize);
   }, []);
-
-  useEffect(() => {
-    setDevice(
-      screenWidth <= 810 ? 'mobile' :
-        screenWidth <= 1200 ? 'tablet' : 'desktop'
-    );
-  }, [screenWidth]);
 
   const context: IDeviceContext = {
     device,
