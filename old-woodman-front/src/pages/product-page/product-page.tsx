87a6495f1f5f75394eb9169d2193e5c7d9db5427
web-developer108 <o.belaya108@gmale.com>
2025-07-15 05:06:12 +0300

    classica-page | added text-info block

--- old-woodman-front/src/pages/product-page/product-page.tsx
+++ old-woodman-front/src/pages/product-page/product-page.tsx
@@ -1,20 +1,21 @@
 import React, { useMemo } from 'react';
-import { useParams} from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import i18n from 'i18next';
 //import { usePageTranslate } from '../../hooks/page-translate/page-translate.ts';
-import { productCatalog}  from '../../config/products.config.ts';
+import { productCatalog } from '../../config/products.config.ts';
 import type { ProductCollection } from '../../config/config.types.ts'
 import { DoorsDetails } from '../../components/doors-details/doors-details.tsx';
-import i18n from 'i18next';
 import { ToolPageLayout } from '../../components/tool-page-layout/tool-page-layout.tsx';
-import styles from './product-page.module.scss';
 import { Breadcrumbs } from '../../components/breadcrumbs/breadcrumbs.tsx';
 import { ShareButton } from '../../components/buttons/share-button/share-button';
+import { TextInfo } from '../../components/text-info/text-info.tsx';
+import styles from './product-page.module.scss';
 
 const ProductPage: React.FC = () => {
   const { collectionId } = useParams();
 //  const { t } = usePageTranslate();
- // const [searchParams] = useSearchParams();
- // const productId = searchParams.get('productId');
+  // const [searchParams] = useSearchParams();
+  // const productId = searchParams.get('productId');
   const lang = i18n.language as 'ru' | 'kk';
 
   const category = useMemo(() => {
@@ -23,29 +24,31 @@ const ProductPage: React.FC = () => {
     );
   }, [collectionId]);
 
-  const collection:  ProductCollection | undefined = useMemo(() => {
+  const collection: ProductCollection | undefined = useMemo(() => {
     return productCatalog.find((category) =>
       category.collections?.some((c) => c.id === collectionId)
     )?.collections?.find((c) => c.id === collectionId);
   }, [collectionId]);
 
-if (!collection){
-  return
-}
+  if (!collection) {
+    return
+  }
 
   return (
     <ToolPageLayout>
       <div className={styles.pageContainer}>
-<div className={styles.topContainer}>
-  <Breadcrumbs
-    current={collection.title[lang]}
-    categoryTitle={category?.title[lang]}
-    categoryId={category?.id}
-  />
-  <ShareButton/>
-</div>
-      <DoorsDetails/>
-      {/* тут будет рендер всей страницы */}
+        <div className={styles.topContainer}>
+          <Breadcrumbs
+            current={collection.title[lang]}
+            categoryTitle={category?.title[lang]}
+            categoryId={category?.id}
+          />
+          <ShareButton/>
+        </div>
+        <DoorsDetails/>
+        <section className={styles.textInfo}>
+          <TextInfo/>
+        </section>
       </div>
     </ToolPageLayout>
   );
