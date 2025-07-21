d803dc107890c46193dad37d5f2b4dbcae99ba81
web-developer108 <o.belaya108@gmale.com>
2025-07-21 21:45:26 +0300

    doors-details | added slider with doors

--- old-woodman-front/src/components/doors-details/doors-details.tsx
+++ old-woodman-front/src/components/doors-details/doors-details.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from 'react';
+import SimpleBar from 'simplebar-react';
+import 'simplebar-react/dist/simplebar.min.css';
 import { useTranslation } from 'react-i18next';
 import { useCart } from '../../hooks/cart/cart.tsx';
 import { LikeButton } from '../buttons/like-button/like-button.tsx';
@@ -13,6 +15,13 @@ import { useModal } from '../../hooks/modal/use-modal.ts';
 import { OneClickModal } from '../modal-windows/one-click-order/one-click-order.tsx';
 import { CartModal } from '../modal-windows/cart-modal/cart-modal.tsx';
 import { CommonButtonsBlock } from '../buttons/common-buttons-block/common-buttons-block.tsx';
+import heroImageClassica from '@assets/images/doors/classica/classica-hero-wide.webp';
+import heroImageLoft from '@assets/images/doors/loft/loft-hero.webp';
+import heroImageDeco from '@assets/images/doors/deco/deco-hero.webp';
+import heroImageCabinet from '@assets/images/doors/cabinet/cabinet-hero.webp';
+import heroImageRustic from '@assets/images/doors/rustic/rustic-hero.webp';
+import heroImageExclusive from '@assets/images/doors/exclusive/exclusive-hero.webp';
+import heroImageBalcony from '@assets/images/doors/balcony/balcony-hero.webp';
 import styles from './doors-details.module.scss';
 
 export const DoorsDetails: React.FC = () => {
@@ -21,19 +30,20 @@ export const DoorsDetails: React.FC = () => {
   const { isInCart, addToCart } = useCart();
   const { showModal } = useModal()
   const navigate = useNavigate();
-  const [isExpanded, setIsExpanded] = useState(false);
-  const [isOverflowing, setIsOverflowing] = useState(false);
-  const textRef = useRef<HTMLDivElement>(null);
   const { collectionId } = useParams();
   const [searchParams] = useSearchParams();
-  const productId = searchParams.get('productId');
+  const textRef = useRef<HTMLDivElement>(null);
+  const simpleBarRef = useRef<any>(null);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isOverflowing, setIsOverflowing] = useState(false);
   const lang = i18n.language as 'ru' | 'kk';
+
+  const productId = searchParams.get('productId');
   const collection = productCatalog
     .find((cat) => cat.id === 'doors')
     ?.collections?.find((col) => col.id === collectionId);
 
   const items = collection?.items || [];
-  console.log(' DoorsDetails')
 
   const selectedProduct = useMemo(() => {
     return productId
@@ -50,6 +60,18 @@ export const DoorsDetails: React.FC = () => {
     addToCart(selectedProduct.id)
     showModal(<CartModal id={selectedProduct.id}/>);
   };
+  const doorCollections = [
+    { id: 'classica', src: heroImageClassica },
+    { id: 'loft', src: heroImageLoft },
+    { id: 'deco', src: heroImageDeco },
+    { id: 'cabinet', src: heroImageCabinet },
+    { id: 'rustic', src: heroImageRustic },
+    { id: 'exclusive', src: heroImageExclusive },
+    { id: 'balcony', src: heroImageBalcony },
+  ];
+  const filteredCollections = doorCollections.filter(
+    (item) => item.id !== collection?.id
+  );
   useEffect(() => {
     const checkOverflow = () => {
       const el = textRef.current;
@@ -162,6 +184,17 @@ export const DoorsDetails: React.FC = () => {
             <span>{t('parameter-header.color.text')}</span>
           </div>
         </div>
+      </section>
+      <section className={styles.slider}>
+        <SimpleBar  ref={simpleBarRef} className={styles.wrapper} autoHide={false}>
+          <div className={styles.track}>
+            {
+              filteredCollections.map((img, index) => (
+                <img key={index} src={img.src} alt={img.id} className={styles.image} loading="lazy"/>
+              ))
+            }
+          </div>
+        </SimpleBar>
       </section>
     </>
   );
