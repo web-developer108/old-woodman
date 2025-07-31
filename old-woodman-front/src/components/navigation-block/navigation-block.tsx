9bdc059462e1c658f8376e1555333fd33b6faf0f
web-developer108 <o.belaya108@gmale.com>
2025-07-31 21:47:19 +0300

    nav-blocks | added facades nav

--- old-woodman-front/src/components/navigation-block/navigation-block.tsx
+++ old-woodman-front/src/components/navigation-block/navigation-block.tsx
@@ -7,9 +7,10 @@ import { useTranslation } from 'react-i18next';
 import useDevice from '../../hooks/device/use-device.ts';
 import door1 from '@assets/images/home/door-1.webp';
 import door2 from '@assets/images/home/door-2.webp';
+import facade1 from '@assets/images/home/facade-1.webp';
+import facade2 from '@assets/images/home/facade-2.webp';
 import shelf from '@assets/images/home/furniture-1.webp';
 import console from '@assets/images/home/furniture-2.webp';
-
 import { AppColors } from '../../styles.ts';
 import styles from './navigation-block.module.scss'
 
@@ -18,16 +19,15 @@ export const NavigationBlock: React.FC<NavigationBlockProps> = ({
   isHome = false,
 }) => {
   const { t } = useTranslation('common');
-  const {  screenWidth} = useDevice();
+  const { screenWidth } = useDevice();
   const navigate = useNavigate();
-
 
   return (
     <div className={`${styles.navigation}  ${!isHome && screenWidth >= 1200 ? styles.navigationCustom : ''}`}>
       {blocks.includes('doors') && (
         <div className={`${styles.background} ${styles.brown}`}>
-            <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title1').toUpperCase()}</h3>
-            <div className={`${styles.bgDescription} ${styles.light}`} >{t('background-description1')}</div>
+          <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title1').toUpperCase()}</h3>
+          <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description1')}</div>
 
           <div className={styles.buttonContainer}>
             <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
@@ -46,10 +46,10 @@ export const NavigationBlock: React.FC<NavigationBlockProps> = ({
           <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description2')}</div>
 
           <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
+            <CircleButton ariaLabel={t('aria-label.furniture')} icon={<ArrowRightIcon/>}
                           bgColor={AppColors.background.circleButton40}
                           onClick={() => {
-                            navigate('/doors');
+                            navigate('/furniture');
                           }}/>
           </div>
           <img className={styles.imageBig} src={shelf} alt="Полка"/>
@@ -57,32 +57,22 @@ export const NavigationBlock: React.FC<NavigationBlockProps> = ({
         </div>
 
       )}
-      {/* {blocks.includes('furniture') && (
-        <div className={styles.backgroundBlue}>
-          <div className={styles.bgContentWrapper}>
-            <h3>{t('background-title2').toUpperCase()}</h3>
-            <div>{t('background-description2')}</div>
-          </div>
-          <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.furniture')} icon={<ArrowRightIcon/>}
-                          bgColor={AppColors.background.circleButton40}
-                          onClick={() => navigate('/furniture')}/>
-          </div>
-        </div>
-      )}
       {blocks.includes('facades') && (
-        <div className={styles.backgroundYellow}>
-          <div className={styles.bgContentWrapperLast}>
-            <h3>{t('background-title3').toUpperCase()}</h3>
-            <div>{t('background-description3')}</div>
-          </div>
+        <div className={`${styles.background} ${styles.yellow}`}>
+          <h3 className={styles.bgTitle}>{t('background-title3').toUpperCase()}</h3>
+          <div className={styles.bgDescription}>{t('background-description3')}</div>
+
           <div className={styles.buttonContainer}>
             <CircleButton ariaLabel={t('aria-label.facades')} icon={<ArrowRightIcon/>}
                           bgColor={AppColors.background.circleButton40}
-                          onClick={() => navigate('/facades')}/>
+                          onClick={() => {
+                            navigate('/facades');
+                          }}/>
           </div>
+          <img className={styles.imageSmall} src={facade1} alt="Панель"/>
+          <img className={styles.imageBig} src={facade2} alt="Панель"/>
         </div>
-      )}*/}
+      )}
     </div>
   );
 };
