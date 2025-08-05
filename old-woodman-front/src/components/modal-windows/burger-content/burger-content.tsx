9b2197d01e9ad8f370dcea05807c4f5f77c6d5b9
web-developer108 <o.belaya108@gmale.com>
2025-08-05 11:06:54 +0300

    improvements-2 | corrected rules for link in burger

--- old-woodman-front/src/components/modal-windows/burger-content/burger-content.tsx
+++ old-woodman-front/src/components/modal-windows/burger-content/burger-content.tsx
@@ -8,7 +8,7 @@ import styles from './burger-content.module.scss';
 export const BurgerContent: React.FC<BurgerContentProps> = ({ onClose }) => {
   const { t } = useTranslation('common');
   const location = useLocation();
-  const prevPathRef = useRef(location.pathname);
+  const prevPathRef = useRef({ pathname: location.pathname, hash: location.hash });
 
   useEffect(() => {
     const handleKey = (e: KeyboardEvent) => {
@@ -17,30 +17,45 @@ export const BurgerContent: React.FC<BurgerContentProps> = ({ onClose }) => {
     window.addEventListener('keydown', handleKey);
     return () => window.removeEventListener('keydown', handleKey);
   }, []);
+
   useEffect(() => {
-    if (location.pathname !== prevPathRef.current) {
+    const { pathname, hash } = location;
+    const prev = prevPathRef.current;
+
+    if (pathname !== prev.pathname || hash !== prev.hash) {
       onClose();
     }
-    prevPathRef.current = location.pathname;
-  }, [location.pathname]);
+
+    prevPathRef.current = { pathname, hash };
+  }, [location.pathname, location.hash]);
+
+  const handleLinkClick = (path: string) => {
+    const url = new URL(path, window.location.origin);
+    const isSamePath = url.pathname === location.pathname;
+    const isSameHash = url.hash === location.hash;
+
+    if (isSamePath && isSameHash) {
+      onClose();
+    }
+  };
 
   return (
     <div className={styles.burgerMenu}>
 
       <div className={styles.navigation}>
-        <Link to="/doors">{t('footer.doors').toUpperCase()}</Link>
-        <Link to="/furniture">{t('footer.furniture').toUpperCase()}</Link>
-        <Link to="/facades">{t('facades-panels').toUpperCase()}</Link>
-        <Link to="/gifts">{t('gifts-decor').toUpperCase()}</Link>
-        <Link to="/favorites">{t('favorites').toUpperCase()}</Link>
-        <Link to="/cart">{t('cart').toUpperCase()}</Link>
-        <Link to="/promotions">{t('promotions').toUpperCase()}</Link>
+        <Link to="/doors"  onClick={() => handleLinkClick('/doors')}>{t('footer.doors').toUpperCase()}</Link>
+        <Link to="/furniture" onClick={() => handleLinkClick('/furniture')}>{t('footer.furniture').toUpperCase()}</Link>
+        <Link to="/facades" onClick={() => handleLinkClick('/facades')}>{t('facades-panels').toUpperCase()}</Link>
+        <Link to="/gifts" onClick={() => handleLinkClick('/gifts')}>{t('gifts-decor').toUpperCase()}</Link>
+        <Link to="/favorites" onClick={() => handleLinkClick('/favorites')}>{t('favorites').toUpperCase()}</Link>
+        <Link to="/cart" onClick={() => handleLinkClick('/cart')}>{t('cart').toUpperCase()}</Link>
+        <Link to="/promotions" onClick={() => handleLinkClick('/promotions')}>{t('promotions').toUpperCase()}</Link>
       </div>
       <div className={styles.footerLinks}>
-        <Link to="/#faq">{t('footer.questions')}</Link>
-        <Link to="/info">{t('footer.info')}</Link>
-        <Link to="/terms">{t('footer.terms')}</Link>
-        <Link to="/contacts">{t('contacts')}</Link>
+        <Link to="/#faq" onClick={() => handleLinkClick('/#faq')}>{t('footer.questions')}</Link>
+        <Link to="/info" onClick={() => handleLinkClick('/info')}>{t('footer.info')}</Link>
+        <Link to="/terms" onClick={() => handleLinkClick('/terms')}>{t('footer.terms')}</Link>
+        <Link to="/contacts"onClick={() => handleLinkClick('/contacts')}>{t('contacts')}</Link>
       </div>
       <div className={styles.social}>
         <SocialPanel/>
