dc878cde319e04af66da91d0b3f69da805c34023
web-developer108 <o.belaya108@gmail.com>
2025-09-10 15:58:03 +0300

    improvments-25 | corrected layout navigation block

--- old-woodman-front/src/components/navigation-block/navigation-block.tsx
+++ old-woodman-front/src/components/navigation-block/navigation-block.tsx
@@ -17,81 +17,84 @@ import { AppColors } from '../../styles.ts';
 import styles from './navigation-block.module.scss'
 
 export const NavigationBlock: React.FC<NavigationBlockProps> = ({
-  blocks = ['doors', 'furniture', 'facades', 'gifts'],
-  isHome = false,
+    blocks = ['doors', 'furniture', 'facades', 'gifts'],
+    isHome = false,
 }) => {
-  const { t } = useTranslation('common');
-  const { screenWidth } = useDevice();
-  const navigate = useNavigate();
+    const {
+        t,
+        i18n
+    } = useTranslation('common');
+    const {screenWidth} = useDevice();
+    const navigate = useNavigate();
+    const lang = i18n.language as 'ru' | 'kk';
+    const kazah = lang ==='kk';
 
-  return (
-    <div className={`${styles.navigation}  ${!isHome && screenWidth >= 1200 ? styles.navigationCustom : ''}`}>
-      {blocks.includes('doors') && (
-        <div className={`${styles.background} ${styles.brown}`}>
-          <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title1').toUpperCase()}</h3>
-          <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description1')}</div>
+    return (<div className={`${styles.navigation}  ${!isHome && screenWidth >= 1200 ? styles.navigationCustom : ''}`}>
+        {blocks.includes('doors') && (<div className={`${styles.background} ${styles.brown} ${kazah ? styles.kazah : ''}`}>
+            <h3 className={`${styles.bgTitle} ${styles.light}  `}>{t('background-title1').toUpperCase()}</h3>
+            <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description1')}</div>
 
-          <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
-                          bgColor={AppColors.background.circleButton40}
-                          onClick={() => {
-                            navigate('/doors');
-                          }}/>
-          </div>
-          <img className={styles.imageSmall} src={door1} loading={'lazy'} alt="Деревянная дверь со стеклопакетом на коричневом фоне"/>
-          <img className={styles.imageBig} src={door2} loading={'lazy'} alt="Белая классическа дверь с наличником на коричневом фоне"/>
-        </div>
-      )}
-      {blocks.includes('furniture') && (
-        <div className={`${styles.background} ${styles.blue}`}>
-          <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title2').toUpperCase()}</h3>
-          <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description2')}</div>
+            <div className={styles.buttonContainer}>
+                <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
+                              bgColor={AppColors.background.circleButton40}
+                              onClick={() => {
+                                  navigate('/doors');
+                              }}/>
+            </div>
+            <img className={styles.imageSmall} src={door1} loading={'lazy'}
+                 alt="Деревянная дверь со стеклопакетом на коричневом фоне"/>
+            <img className={styles.imageBig} src={door2} loading={'lazy'}
+                 alt="Белая классическая дверь с наличником на коричневом фоне"/>
+        </div>)}
+        {blocks.includes('furniture') && (<div className={`${styles.background} ${styles.blue} ${kazah ? styles.kazah : ''}`}>
+                <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title2').toUpperCase()}</h3>
+                <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description2')}</div>
 
-          <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.furniture')} icon={<ArrowRightIcon/>}
-                          bgColor={AppColors.background.circleButton40}
-                          onClick={() => {
-                            navigate('/furniture');
-                          }}/>
-          </div>
-          <img className={styles.imageBig} src={shelf} loading={'lazy'} alt="Книжная полка из бука, двойная, на голубом фоне"/>
-          <img className={styles.imageSmall} src={console} loading={'lazy'}
-               alt="Деревянная консоль с винтажной окраской, на голубом фоне"/>
-        </div>
+                <div className={styles.buttonContainer}>
+                    <CircleButton ariaLabel={t('aria-label.furniture')} icon={<ArrowRightIcon/>}
+                                  bgColor={AppColors.background.circleButton40}
+                                  onClick={() => {
+                                      navigate('/furniture');
+                                  }}/>
+                </div>
+                <img className={styles.imageBig} src={shelf} loading={'lazy'}
+                     alt="Книжная полка из бука, двойная, на голубом фоне"/>
+                <img className={styles.imageSmall} src={console} loading={'lazy'}
+                     alt="Деревянная консоль с винтажной окраской, на голубом фоне"/>
+            </div>
 
-      )}
-      {blocks.includes('facades') && (
-        <div className={`${styles.background} ${styles.yellow}`}>
-          <h3 className={styles.bgTitle}>{t('background-title3').toUpperCase()}</h3>
-          <div className={styles.bgDescription}>{t('background-description3')}</div>
+        )}
+        {blocks.includes('facades') && (<div className={`${styles.background} ${styles.yellow}`}>
+            <h3 className={styles.bgTitle}>{t('background-title3').toUpperCase()}</h3>
+            <div className={styles.bgDescription}>{t('background-description3')}</div>
 
-          <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.facades')} icon={<ArrowRightIcon/>}
-                          bgColor={AppColors.background.circleButton40}
-                          onClick={() => {
-                            navigate('/facades');
-                          }}/>
-          </div>
-          <img className={styles.imageSmall} src={facade1} loading={'lazy'} alt="Декоративная стеновая панель с готической росписью"/>
-          <img className={styles.imageBig} src={facade2} loading={'lazy'} alt="Деревянный мебельный фасад из массива соны "/>
-        </div>
-      )}
-      {blocks.includes('gifts') && (
-        <div className={`${styles.background} ${styles.orange}`}>
-          <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title4').toUpperCase()}</h3>
-          <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description4')}</div>
+            <div className={styles.buttonContainer}>
+                <CircleButton ariaLabel={t('aria-label.facades')} icon={<ArrowRightIcon/>}
+                              bgColor={AppColors.background.circleButton40}
+                              onClick={() => {
+                                  navigate('/facades');
+                              }}/>
+            </div>
+            <img className={styles.imageSmall} src={facade1} loading={'lazy'}
+                 alt="Декоративная стеновая панель с готической росписью"/>
+            <img className={styles.imageBig} src={facade2} loading={'lazy'}
+                 alt="Деревянный мебельный фасад из массива сосны "/>
+        </div>)}
+        {blocks.includes('gifts') && (<div className={`${styles.background} ${styles.orange}`}>
+            <h3 className={`${styles.bgTitle} ${styles.light}`}>{t('background-title4').toUpperCase()}</h3>
+            <div className={`${styles.bgDescription} ${styles.light}`}>{t('background-description4')}</div>
 
-          <div className={styles.buttonContainer}>
-            <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
-                          bgColor={AppColors.background.circleButton40}
-                          onClick={() => {
-                            navigate('/gifts');
-                          }}/>
-          </div>
-          <img className={styles.imageSmall} src={gift1} loading={'lazy'} alt="Деревянный светильник эксклюзивный подарок алматы"/>
-          <img className={styles.imageBig} src={gift2} loading={'lazy'} alt="Эксклюзивные часы из древесины карагача"/>
-        </div>
-      )}
-    </div>
-  );
+            <div className={styles.buttonContainer}>
+                <CircleButton ariaLabel={t('aria-label.door')} icon={<ArrowRightIcon/>}
+                              bgColor={AppColors.background.circleButton40}
+                              onClick={() => {
+                                  navigate('/gifts');
+                              }}/>
+            </div>
+            <img className={styles.imageSmall} src={gift1} loading={'lazy'}
+                 alt="Деревянный светильник эксклюзивный подарок алматы"/>
+            <img className={styles.imageBig} src={gift2} loading={'lazy'}
+                 alt="Эксклюзивные часы из древесины карагача"/>
+        </div>)}
+    </div>);
 };
